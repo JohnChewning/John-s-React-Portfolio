11f77d186eb4d30cd12f8497e9639dd9fa082272
JohnChewning <johnathanchewning@gmail.com>
2024-04-28 13:54:26 -0400

    created the navbar and introduction components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/navbar';
+import Intro from './components/Navbar/Intro/intro';
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Intro/>
     </div>
   )
 }
